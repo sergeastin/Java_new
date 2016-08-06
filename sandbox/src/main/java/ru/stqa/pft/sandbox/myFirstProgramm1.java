9970a5830c9f4d4d5dfb154b02d5519240cdb8af
sergeastin <sergeastin@gmail.com>
2016-08-06 19:16:10 -0400

    created packet for first my prog

--- sandbox/src/main/java/ru/stqa/pft/sandbox/myFirstProgramm1.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/myFirstProgramm1.java
@@ -1,4 +1,6 @@
-public class myFirstProgramm {
+package ru.stqa.pft.sandbox;
+
+public class myFirstProgramm1 {
 	
 	public static void main(String [] args){
 	    System.out.println("Hello,world");
