aefcdf3841e938ffb1b79d24b34bd60c608bdb75
sergeastin <sergeastin@gmail.com>
2016-08-10 15:57:31 -0400

    1.15

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Cj on 8/10/2016.
+ */
+public class Square {
+    public double l;
+    public Square(double l){
+        this.l=l;
+
+    }
+    public  double area(){
+        return this.l*this.l;
+    }
+}
--- sandbox/src/main/java/ru/stqa/pft/sandbox/myFirstProgramm1.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/myFirstProgramm1.java
@@ -4,21 +4,19 @@ public class myFirstProgramm1 {
   public static void main(String [] args){
     hello("World");
 	hello(" ");
-    double l=5;
-	  System.out.println("ploshad kvadrata so storonoj "+l+"="+area(l));
+	  Square s= new Square(5);
 
-	  double a=4;
-	  double b=6;
-	  System.out.println("ploshad pryamougolnika so storonami "+a+" i "+b+"="+area(a,b));
+
+
+	  System.out.println("ploshad kvadrata so storonoj "+s.l+"="+s.area());
+	  Rectangle r= new Rectangle(4,6);
+
+	  System.out.println("ploshad pryamougolnika so storonami "+r.a+" i "+r.b+"="+r.area());
 	}
 	public static void hello(String somebody){
 
 		System.out.println("Hello,"+somebody+"!");
 	}
-	public static double area(double t){
-		 return t*t;
-	}
-	public static double area(double a,double b){
-		return a*b;
-	}
+
+
 }
